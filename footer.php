0dab302b669400d7e8759018be77781587647865
François Risoud <francois.risoud@gmail.com>
2021-04-18 22:28:30 +0200

    style footer

--- footer.php
+++ footer.php
@@ -1,10 +1,12 @@
 </div>
 <footer id="footer">
   <div class="background"></div>
-  <div id="copyright">
-  &copy; <?php echo esc_html( date_i18n( __( 'Y', 'assomast' ) ) ); ?> <?php echo esc_html( get_bloginfo( 'name' ) ); ?>
+  <div class="content">
+    <?php wp_nav_menu(array('theme_location' => 'footer-menu')); ?>
+    <div id="copyright">
+    &copy; <?php echo esc_html( date_i18n( __( 'Y', 'assomast' ) ) ); ?> <?php echo esc_html( get_bloginfo( 'name' ) ); ?>
+    </div>
   </div>
-  <?php wp_nav_menu(array('theme_location' => 'footer-menu')); ?>
 </footer>
 </div>
 <?php wp_footer(); ?>
