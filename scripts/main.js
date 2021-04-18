465513c2d012e35df9e629d7d510fd89c6b08e17
François Risoud <francois.risoud@gmail.com>
2021-04-18 20:50:30 +0200

    comments

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,13 @@
 (() => {
+  // MOUSE
   const root = document.documentElement;
   const navElement = document.querySelector('#menu');
-
   root.addEventListener('mousemove', e => {
     root.style.setProperty('--mouse-x', e.clientX + 'px');
     root.style.setProperty('--mouse-y', e.clientY + 'px');
   });
 
+  // NAV
   function setNavClass() {
     if (window.scrollY > 10) {
       navElement.classList.add('nav--plain');
@@ -14,10 +15,8 @@
       navElement.classList.remove('nav--plain');
     }
   }
-
   window.addEventListener('scroll', e => {
     setNavClass();
   });
-
   setNavClass();
 })();
